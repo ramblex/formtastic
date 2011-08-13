097b368342de6ac19f651623624d0cb58c536ffd
Alex Duller <alexduller@gmail.com>
2011-08-13 19:22:48 +0100

    Don't add model name to form class

--- lib/formtastic/helpers/form_helper.rb
+++ lib/formtastic/helpers/form_helper.rb
@@ -148,15 +148,6 @@ module Formtastic
 
         singularizer = defined?(ActiveModel::Naming.singular) ? ActiveModel::Naming.method(:singular) : ActionController::RecordIdentifier.method(:singular_class_name)
 
-        class_names = options[:html][:class] ? options[:html][:class].split(" ") : []
-        class_names << @@default_form_class
-        class_names << case record_or_name_or_array
-          when String, Symbol then record_or_name_or_array.to_s                                  # :post => "post"
-          when Array then options[:as] || singularizer.call(record_or_name_or_array.last.class)  # [@post, @comment] # => "comment"
-          else options[:as] || singularizer.call(record_or_name_or_array.class)                  # @post => "post"
-        end
-        options[:html][:class] = class_names.join(" ")
-
         with_custom_field_error_proc do
           self.form_for(record_or_name_or_array, *(args << options), &proc)
         end
